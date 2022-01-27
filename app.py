e7896bae75c7240be3e7aae805f0787e091fe1cc
Shubham Luharuka <luharukashubham4@gmail.com>
2022-01-27 21:54:37 +0530

    Updated final Version

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 #####################Import all Modules ########################################
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,session
 from rasa.core.agent import Agent
 import asyncio
 import nest_asyncio
 from typing import Text
 import tensorflow as tf
+
 
 ################### Declare Application #############################################
 app = Flask(__name__)
@@ -13,40 +14,48 @@ app = Flask(__name__)
 
 with tf.device("cpu:0"):
     nest_asyncio.apply()
-    modelPath=r"C:\Users\luhar\Projects\Covid19-Chatbot\models\20220123-135605-allegro-ridge.tar.gz"
+    modelPath="./static/20220123-135605-allegro-ridge.tar.gz"
     agent = Agent.load(str(modelPath))
     async def parse(text: Text):
         global agent
         response = await agent.handle_text(text)
         return response
 
+message_hist={}
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
+
 def home():
-    return render_template('index.html')
+    
 
-
-@app.route("/start", methods=['GET','POST'])
-def start():
     ret=""
-    msg="Hello"
+    msg=""
+
     if request.method=='POST':
         msg=request.form['fname']
+        if agent.is_ready():
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            response = loop.run_until_complete(parse(msg))
+        if response is not None :
+            if type(response) is list:
+                ret=response[0].get('text')
 
-    if agent.is_ready():
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        response = loop.run_until_complete(parse(msg))
-
-    if response is not None :
-        if type(response) is list:
-            ret=response[0].get('text')
+            else:
+                ret=response
+                
+            message_hist[msg]=ret
+            print(message_hist)
         else:
-            ret=response
-    else:
-        ret='The response was not valid'
+            ret='The response was not valid'
 
-    return render_template('index.html',message=msg,response=ret)
+        return render_template('index.html',message_hist=message_hist)
+
+    else:
+        
+        return render_template('index.html')
+
 
 if __name__=="__main__":
     app.run(debug= True)
+    message_hist={}
